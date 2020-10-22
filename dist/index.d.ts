7b92ed5bb647303abe1fdf0736d25a87f082f37a
JeffGreat <jf.gruet@gmail.com>
2020-10-23 00:10:58 +0200

    update index

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,8 +2,4 @@ export * from './base-model';
 export * from './api';
 export * from './auth';
 export * from './user';
-export * from './page-seo';
-export * from './business';
-export * from './offer';
-export * from './booking';
 export * from './constants';
--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,4 @@ function __export(m) {
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./user"));
-__export(require("./business"));
-__export(require("./offer"));
-__export(require("./booking"));
 __export(require("./constants"));
