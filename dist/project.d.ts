176e944ba5da129500b1740f4ccf534363878001
JeffGreat <jf.gruet@gmail.com>
2020-10-24 23:58:37 +0200

    build

--- dist/project.d.ts
+++ dist/project.d.ts
@@ -1,22 +1,14 @@
 import { IBaseModel } from './base-model';
 export interface IFolio extends IBaseModel {
     modelData: {
-        units: string;
-        unitsAbbreviation: string;
-        unitsConversionFactor: number;
-        gridSize: number;
-        wallThickness: number;
-        preferences: {
-            showWallGuidelines: boolean;
-            showWallLengths: boolean;
-            showWallAngles: boolean;
-            showOnlySmallWallAngles: boolean;
-            showGrid: boolean;
-            gridSnap: boolean;
-        };
+        [index: string]: any;
     };
-    nodeDataArray: any[];
-    linkDataArray: any[];
+    nodeDataArray: {
+        [index: string]: any;
+    }[];
+    linkDataArray: {
+        [index: string]: any;
+    }[];
 }
 export interface IProject extends IBaseModel {
     name: string;
