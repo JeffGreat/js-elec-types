ddd9d92d4baed1b81b4a388c908f50d27e1148a9
JeffGreat <jf.gruet@gmail.com>
2020-10-24 23:58:12 +0200

    type folio props

--- src/project.ts
+++ src/project.ts
@@ -1,23 +1,9 @@
 import { IBaseModel } from './base-model';
 
 export interface IFolio extends IBaseModel {
-    modelData: {
-        units: string;
-        unitsAbbreviation: string;
-        unitsConversionFactor: number;
-        gridSize: number;
-        wallThickness: number;
-        preferences: {
-            showWallGuidelines: boolean;
-            showWallLengths: boolean;
-            showWallAngles: boolean;
-            showOnlySmallWallAngles: boolean;
-            showGrid: boolean;
-            gridSnap: boolean;
-        };
-    };
-    nodeDataArray: any[];
-    linkDataArray: any[];
+    modelData: { [index: string]: any };
+    nodeDataArray: { [index: string]: any }[];
+    linkDataArray: { [index: string]: any }[];
 }
 
 export interface IProject extends IBaseModel {
